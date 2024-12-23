bc93776b9526ca0550293d13b5d4970443d8e9b5
Jayan Wadusinghearachchi <jayan.wadusinghearachchi@kognitiv.com>
2024-12-23 11:46:06 +0530

    Webpack distribution configured

---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  optimization: {
+    minimize: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        use: ['file-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new Dotenv({
+        path: 'env/production.env',
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    port: 3000,
+    hot: true,
+    open: true,
+    historyApiFallback: true,
+  },
+  mode: 'production',
+  performance: {
+    hints: 'warning',
+    maxEntrypointSize: 1200000,
+    maxAssetSize: 1200000,
+  },
+};
